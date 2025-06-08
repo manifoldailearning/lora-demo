4e785f19cdb3e1308ff13fc2b0882f8258a5c156
Nachiketh <murthy@manifoldailearning.in>
2025-06-08 10:08:11 +0530

    fix

---
+++ fast-api-demo/README.md
@@ -0,0 +1,7 @@
+# Core Elements of FastAPI
+- @app.get() - Define a GET endpoint
+- @app.post() - Define a POST endpoint
+- BaseModel - Pydantic Model for data validation
+- async def - Support async i/o for better performance
+- /docs - Swagger UI (auto-generated)
+- /redoc - alternate documentation
--- fast-api-demo/app.py
+++ fast-api-demo/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+import asyncio
+import time
 
 app = FastAPI(title="Demo App")
 
@@ -16,6 +18,16 @@ def read_root():
 @app.post("/items")
 def create_item(item: demo_item):
     return {"item_name": item.name, "item_price": item.price, "item_offer": item.is_offer}
+
+@app.get("/sync")
+def wait():
+    time.sleep(5) # block the server for 5 seconds
+    return {"message": "Sync done after 5 seconds"}
+
+@app.get("/async")
+async def async_wait():
+    await asyncio.sleep(5) # non blocking wait for 5 seconds
+    return {"message": "Async Sync done after 5 seconds"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)  # Run the app on all interfaces at port 8000
